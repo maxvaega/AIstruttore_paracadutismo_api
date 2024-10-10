e8e03ca055dcf81800ca026084356a9fb5efdc9e
diego tonini <diego.tonini93@gmail.com>
2024-10-10 17:18:32 +0200

    debug 2

--- api/fetchRunRecursive.ts
+++ api/fetchRunRecursive.ts
@@ -54,23 +54,23 @@ export async function POST(request: Request) {
   }
 
   // completed
-
-  try {
-    const lastMessage = await client.popLastMessage(runId);
-    const messageType = lastMessage.content[0].type;
-    if (messageType === "text") {
-      const answer = lastMessage.content[0].text.value.slice(0, 1000);
-      waitUntil(sendMessageToUser(personId, answer));
-    } else {
-      waitUntil(notifyError());
-      throw new Error(
-        `runId: ${runId}, lastMessage was of type ${messageType}`
-      );
-    }
-  } catch (e) {
-    waitUntil(notifyError());
-    return new Response("error generic", { status: 400 });
-  }
+  console.log(threadId, "has completed");
+  // try {
+  //   const lastMessage = await client.popLastMessage(runId);
+  //   const messageType = lastMessage.content[0].type;
+  //   if (messageType === "text") {
+  //     const answer = lastMessage.content[0].text.value.slice(0, 1000);
+  //     waitUntil(sendMessageToUser(personId, answer));
+  //   } else {
+  //     waitUntil(notifyError());
+  //     throw new Error(
+  //       `runId: ${runId}, lastMessage was of type ${messageType}`
+  //     );
+  //   }
+  // } catch (e) {
+  //   waitUntil(notifyError());
+  //   return new Response("error generic", { status: 400 });
+  // }
 
   return new Response("OK", { status: 200 });
 }
