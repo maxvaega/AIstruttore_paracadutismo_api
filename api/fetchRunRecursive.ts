9f32c904bb16899be6c275eb378dd95076d70932
diego tonini <diego.tonini93@gmail.com>
2024-10-10 17:23:42 +0200

    debug 5

--- api/fetchRunRecursive.ts
+++ api/fetchRunRecursive.ts
@@ -59,17 +59,16 @@ export async function POST(request: Request) {
     const lastMessage = await client.popLastMessage(runId);
     const messageType = lastMessage.content[0].type;
     console.log("text message is", lastMessage);
-    // if (messageType === "text") {
-
-    //   const answer = lastMessage.content[0].text.value.slice(0, 1000);
-
-    //   waitUntil(sendMessageToUser(personId, answer));
-    // } else {
-    //   waitUntil(notifyError());
-    //   throw new Error(
-    //     `runId: ${runId}, lastMessage was of type ${messageType}`
-    //   );
-    // }
+    if (messageType === "text") {
+      const answer = lastMessage.content[0].text.value.slice(0, 1000);
+      console.log("answer is", answer);
+      // waitUntil(sendMessageToUser(personId, answer));
+    } else {
+      waitUntil(notifyError());
+      throw new Error(
+        `runId: ${runId}, lastMessage was of type ${messageType}`
+      );
+    }
   } catch (e) {
     waitUntil(notifyError());
     return new Response("error generic", { status: 400 });
